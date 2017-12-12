5cb993692f8d74ceef0fcaf6b3c2e98f4705c373
Nei Santos <nrauni@gmail.com>
2017-12-12 11:03:09 +0000

    update dotfiles

--- bash/.bash_profile.sh
+++ bash/.bash_profile.sh
@@ -1,6 +1,15 @@
 #make Casks install to sensible and predictable locations
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
+# Infinite history
+export HISTSIZE=""
+# Avoid duplicates on history
+export HISTCONTROL=ignoredups:erasedups  
+# When the shell exits, append to the history file instead of overwriting it
+shopt -s histappend
+# Save and reload the history after each command finishes
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+
 # Load the shell dotfiles, and then some:
 # * ~/.path can be used to extend `$PATH`.
 # * ~/.extra can be used for other settings you don’t want to commit.
--- vim/.vimrc
+++ vim/.vimrc
@@ -3,13 +3,6 @@ set background=dark
 " Use UTF-8 without BOM
 set encoding=utf-8 nobomb
 
-" Centralize backups, swapfiles and undo history
-set backupdir=~/.vim/backups
-set directory=~/.vim/swaps
-if exists("&undodir")
-	set undodir=~/.vim/undo
-endif
-
 " Enable line numbers
 set number
 
