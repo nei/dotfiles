8b907ef1f39fe20b935291ae227816e4c704c081
Nei Santos <nrauni@gmail.com>
2019-02-28 14:08:26 +0000

    Update macosdefaults.sh

--- macOS/macosdefaults.sh
+++ macOS/macosdefaults.sh
@@ -3,3 +3,7 @@ defaults write com.apple.finder AppleShowAllFiles -bool true
 
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Disable smart quotes to avoid issues with pasting invalid chars on terminal
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
